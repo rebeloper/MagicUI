2a2f00bf0ff12374813432d5dfa92426ea9ef0f3
rebeloper <therebeloper@gmail.com>
2022-06-17 15:41:25 +0300

    first commit

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "MagicUI",
+    platforms: [
+      .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +21,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "MagicUI",
-            dependencies: []),
-        .testTarget(
-            name: "MagicUITests",
-            dependencies: ["MagicUI"]),
+            path: "./Sources/MagicUI.xcframework")
     ]
 )
---
+++ Sources/MagicUI.xcframework/ios-arm64/MagicUI.framework/Headers/MagicUI.h
@@ -0,0 +1,18 @@
+//
+//  MagicUI.h
+//  MagicUI
+//
+//  Created by Alex Nagy on 17.06.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MagicUI.
+FOUNDATION_EXPORT double MagicUIVersionNumber;
+
+//! Project version string for MagicUI.
+FOUNDATION_EXPORT const unsigned char MagicUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagicUI/PublicHeader.h>
+
+
