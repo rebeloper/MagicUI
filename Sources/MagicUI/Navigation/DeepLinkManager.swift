38051bb309c46de20a0cd0b19bc01bb1242362c9
rebeloper <therebeloper@gmail.com>
2022-08-02 19:08:49 +0300

    isLast

--- Sources/MagicUI/Navigation/DeepLinkManager.swift
+++ Sources/MagicUI/Navigation/DeepLinkManager.swift
@@ -5,13 +5,16 @@
 //  Created by Alex Nagy on 02.08.2022.
 //
 
-import Foundation
+import SwiftUI
 
 public struct DeepLinkManager {
-    public static func step(_ count: Int, isActive: Bool, action: @escaping () -> ()) {
-        if isActive {
+    public static func step(_ count: Int, isActive: Binding<Bool>, isLast: Bool = false, action: @escaping () -> ()) {
+        if isActive.wrappedValue {
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.6 * Double(count), execute: {
                 action()
+                if isLast {
+                    isActive.wrappedValue = false
+                }
             })
         }
     }
