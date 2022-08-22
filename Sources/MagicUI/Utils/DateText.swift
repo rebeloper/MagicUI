412f91b54ad1357a5cd5fb757504bc3781f3387b
rebeloper <therebeloper@gmail.com>
2022-08-22 19:31:05 +0300

    DateText

---
+++ Sources/MagicUI/Utils/DateText.swift
@@ -0,0 +1,45 @@
+//
+//  DateText.swift
+//  
+//
+//  Created by Alex Nagy on 22.08.2022.
+//
+
+import SwiftUI
+
+public struct DateText: View {
+    
+    private let date: Date
+    
+    /// Creates view that displays a date in a text view
+    /// - Parameter date: The date
+    public init(_ date: Date) {
+        self.date = date
+    }
+    
+    private var isLessThanOneMinute: Bool { date.timeIntervalSinceNow > -60 }
+    private var isLessThanOneDay: Bool { date.timeIntervalSinceNow > -60 * 60 * 24 }
+    private var isLessThanOneWeek: Bool { date.timeIntervalSinceNow > -60 * 60 * 24 * 7}
+    private var isLessThanOneYear: Bool { date.timeIntervalSinceNow > -60 * 60 * 24 * 365}
+    
+    public var body: some View {
+        if isLessThanOneMinute {
+            Text(date.formatted(.dateTime.hour().minute().second()))
+        } else {
+            if isLessThanOneDay {
+                Text(date.formatted(.dateTime.hour().minute()))
+            } else {
+                if isLessThanOneWeek {
+                    Text(date.formatted(.dateTime.weekday(.wide).hour().minute()))
+                } else {
+                    if isLessThanOneYear {
+                        Text(date.formatted(.dateTime.month().day()))
+                    } else {
+                        Text(date.formatted(.dateTime.year().month().day()))
+                    }
+                }
+            }
+        }
+    }
+}
+
