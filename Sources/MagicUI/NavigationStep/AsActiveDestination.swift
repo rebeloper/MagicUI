8b999b01190e272cbdb859d587da3630e249003e
rebeloper <therebeloper@gmail.com>
2022-09-20 13:27:57 +0300

    AsActiveDestination

---
+++ Sources/MagicUI/NavigationStep/AsActiveDestination.swift
@@ -0,0 +1,35 @@
+//
+//  AsActiveDestination.swift
+//  
+//
+//  Created by Alex Nagy on 20.09.2022.
+//
+
+import SwiftUI
+
+internal struct AsActiveDestination: ViewModifier {
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    @Binding internal var isActive: Bool
+    
+    internal func body(content: Content) -> some View {
+        content
+            .onChange(of: isActive) { isActive in
+                if !isActive {
+                    dismiss()
+                }
+            }
+    }
+}
+
+public extension View {
+    
+    /// Sets a binding or publisher to a Boolean value that determines whether
+    ///     this view is presented.
+    /// - Parameter isActive: A binding or publisher to a Boolean value that determines whether
+    ///     this view is presented.
+    func setAsActiveDestination(_ isActive: Binding<Bool>) -> some View {
+        self.modifier(AsActiveDestination(isActive: isActive))
+    }
+}
