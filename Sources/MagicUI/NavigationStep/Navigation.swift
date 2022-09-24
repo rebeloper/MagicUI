2e758f675e07fd6eb68ca615609f344c368884c0
rebeloper <therebeloper@gmail.com>
2022-09-24 17:47:58 +0300

    Navigation

--- Sources/MagicUI/NavigationStep/Navigation.swift
+++ Sources/MagicUI/NavigationStep/Navigation.swift
@@ -10,34 +10,29 @@ import SwiftUI
 public struct Navigation {
     
     /// Activates one or more ``NavigationStep``s
-    /// - Parameter path: one or more ``NavigationStep``s
-    public static func activate(_ path: Binding<NavigationStep>...) {
-        if path.count >= 2 {
-            for i in 0..<path.count {
+    /// - Parameter steps: one or more ``NavigationStep``s
+    public static func present(_ steps: Binding<NavigationStep>...) {
+        if steps.count >= 2 {
+            for i in 0..<steps.count {
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.1 + 0.6 * Double(i), execute: {
-                    path[i].isActive.wrappedValue = true
+                    steps[i].isActive.wrappedValue = true
                 })
             }
         } else {
             DispatchQueue.main.async {
-                path[0].isActive.wrappedValue = true
+                steps[0].isActive.wrappedValue = true
             }
         }
     }
     
-    /// Deactivates one or more ``NavigationStep``s
-    /// - Parameter path: one or more ``NavigationStep``s
-    public static func deactivate(_ path: Binding<NavigationStep>...) {
-        if path.count >= 2 {
-            for i in 0..<path.count {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1 + 0.6 * Double(i), execute: {
-                    path[i].isActive.wrappedValue = false
-                })
-            }
-        } else {
-            DispatchQueue.main.async {
-                path[0].isActive.wrappedValue = false
-            }
+    /// Deactivates more than one ``NavigationStep``s
+    /// - Parameter steps: more than one ``NavigationStep``s
+    public static func dismiss(_ steps: Binding<NavigationStep>...) {
+        guard steps.count >= 2 else { fatalError("`steps` must have more than one NavigationStep. If you'd like to dismiss only one NavigationStep, please use the `dismiss()` DismissAction instead.") }
+        for i in 0..<steps.count {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1 + 0.6 * Double(i), execute: {
+                steps[i].isActive.wrappedValue = false
+            })
         }
     }
     
