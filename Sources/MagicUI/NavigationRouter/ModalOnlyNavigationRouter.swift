21f20cf40fb1e9cc1f11f17663a16f781bf0cfc5
rebeloper <therebeloper@gmail.com>
2022-09-29 11:40:56 +0300

    ModalOnlyNavigationRouter

---
+++ Sources/MagicUI/NavigationRouter/ModalOnlyNavigationRouter.swift
@@ -0,0 +1,34 @@
+//
+//  ModalOnlyNavigationRouter.swift
+//  
+//
+//  Created by Alex Nagy on 29.09.2022.
+//
+
+import SwiftUI
+
+/// Creates a navigation stack. Use this if you are planning to use only modals.
+///
+/// - Parameters:
+///   - root: The view to display when the stack is empty.
+public struct ModalOnlyNavigationRouter<Root: View>: View {
+    
+    @StateObject private var router = Router()
+    
+    @ViewBuilder private var root: () -> Root
+    
+    /// Creates a navigation stack. Use this if you are planning to use only modals.
+    ///
+    /// - Parameters:
+    ///   - root: The view to display when the stack is empty.
+    public init(@ViewBuilder root: @escaping () -> Root) {
+        self.root = root
+    }
+    
+    public var body: some View {
+        NavigationStack {
+            root()
+        }
+        .environmentObject(router)
+    }
+}
