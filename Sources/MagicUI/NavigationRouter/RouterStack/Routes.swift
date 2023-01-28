020d05b3c65eb6abc34a1fe34db78784350396ec
rebeloper <therebeloper@gmail.com>
2023-01-28 09:27:50 +0200

    Routes

--- Sources/MagicUI/NavigationRouter/RouterStack/Routes.swift
+++ Sources/MagicUI/NavigationRouter/RouterStack/Routes.swift
@@ -6,6 +6,13 @@
 
 import SwiftUI
 
+public enum PopType {
+    case one
+    case the(last: Int)
+    case to(index: Int)
+    case toRoot
+}
+
 public class Routes<Destination: RouterDestination>: ObservableObject {
     
     @Published public var modalsState = Array(repeating: Array(repeating: false, count: 100), count: 10)
@@ -15,6 +22,14 @@ public class Routes<Destination: RouterDestination>: ObservableObject {
     @Published public var paths = Array(repeating: Array(repeating: NavigationPath(), count: 100), count: 10)
     @Published public var pathIndex = Array(repeating: 0, count: 10)
     
+    
+    // MARK: - push
+    
+    /// Pushes a destination onto the router stack.
+    ///
+    /// - Parameters:
+    ///   - destination: Destination view.
+    ///   - completion: Callback trigerred when the destination finished pushing.
     public func push(_ destination: Destination, completion: @escaping () -> () = {}) {
         guard pathIndex[tabSelection] < paths[tabSelection].count else { return }
         DispatchQueue.main.async {
@@ -25,7 +40,95 @@ public class Routes<Destination: RouterDestination>: ObservableObject {
         })
     }
     
-    public func dismiss(completion: @escaping () -> ()) {
+    /// Pushes a destination onto the router stack asyncronously.
+    ///
+    /// - Parameter destination: Destination view.
+    public func push(_ destination: Destination) async {
+        await withCheckedContinuation({ continuation in
+            push(destination) {
+                continuation.resume()
+            }
+        })
+    }
+    
+    // MARK: - present
+    
+    /// Presents a destination as a modal on the router stack.
+    ///
+    /// - Parameters:
+    ///   - destination: Destination view.
+    ///   - completion: Callback trigerred when the destination finished presenting.
+    public func present(_ destination: Destination, completion: @escaping () -> () = {}) {
+        activeModalsIndices[tabSelection].append(destination.modalValue)
+        DispatchQueue.main.async {
+            self.modalsState[self.tabSelection][destination.modalValue] = true
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.6, execute: {
+            completion()
+        })
+    }
+    
+    /// Presents a destination as a modal on the router stack asyncronously.
+    ///
+    /// - Parameter destination: Destination view.
+    public func present(_ destination: Destination) async {
+        await withCheckedContinuation({ continuation in
+            present(destination) {
+                continuation.resume()
+            }
+        })
+    }
+    
+    // MARK: - pop
+    
+    /// Pops a view from the router stack.
+    ///
+    /// - Parameters:
+    ///   - type: The type of the pop.
+    ///   - completion: Callback trigerred when the destination finished popping.
+    public func pop(_ type: PopType = .one, completion: @escaping () -> () = {}) {
+        switch type {
+        case .one:
+            if paths[tabSelection][pathIndex[tabSelection]].count == 0 {
+                dismissModal(completion: completion)
+            } else {
+                dismiss(completion: completion)
+            }
+        case .the(let last):
+            for i in 0..<last {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.6 * Double(i), execute: {
+                    self.pop {
+                        if i == last - 1 {
+                            completion()
+                        }
+                    }
+                })
+            }
+        case .to(let index):
+            let all = getAllViewsCount()
+            let last = all - index
+            pop(.the(last: last), completion: completion)
+        case .toRoot:
+            let all = getAllViewsCount()
+            let last = all
+            pop(.the(last: last), completion: completion)
+        }
+    }
+    
+    /// Pops a view from the router stack asyncronously.
+    ///
+    /// - Parameter type: The type of the pop.
+    public func pop(_ type: PopType = .one) async {
+        await withCheckedContinuation({ continuation in
+            pop(type) {
+                continuation.resume()
+            }
+        })
+    }
+    
+    // MARK: - internal
+    
+    internal func dismiss(completion: @escaping () -> ()) {
         guard pathIndex[tabSelection] < paths[tabSelection].count, !paths[tabSelection][pathIndex[tabSelection]].isEmpty else { return }
         DispatchQueue.main.async {
             self.paths[self.tabSelection][self.pathIndex[self.tabSelection]].removeLast()
@@ -35,22 +138,24 @@ public class Routes<Destination: RouterDestination>: ObservableObject {
         })
     }
     
-    public func present(_ isActive: Binding<Bool>, completion: @escaping () -> () = {}) {
+    internal func dismissModal(completion: @escaping () -> () = {}) {
+        guard let index = activeModalsIndices[tabSelection].last else { return }
         DispatchQueue.main.async {
-            isActive.wrappedValue = true
+            self.modalsState[self.tabSelection][index] = false
         }
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.6, execute: {
             completion()
         })
     }
     
-    public func dismissModal(_ isActive: Binding<Bool>, completion: @escaping () -> () = {}) {
-        DispatchQueue.main.async {
-            isActive.wrappedValue = false
+    internal func getAllViewsCount() -> Int {
+        var count = 0
+        paths[tabSelection].forEach { path in
+            count += path.count
         }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.6, execute: {
-            completion()
-        })
+        count += activeModalsIndices[tabSelection].count
+        count -= 1
+        return count
     }
     
 }
