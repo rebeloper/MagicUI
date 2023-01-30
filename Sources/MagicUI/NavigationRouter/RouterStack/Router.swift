2cd0067225893af04b8edaf7cc542a47c3994c43
rebeloper <therebeloper@gmail.com>
2023-01-30 15:57:13 +0200

    Router

--- Sources/MagicUI/NavigationRouter/RouterStack/Router.swift
+++ Sources/MagicUI/NavigationRouter/RouterStack/Router.swift
@@ -1,170 +1,3 @@
-////
-////  Router.swift
-////
-////  Created by Alex Nagy on 19.01.2023.
-////
-//
-//import SwiftUI
-//
-//public enum PopType {
-//    case one
-//    case the(last: Int)
-//    case to(index: Int)
-//    case toRoot
-//}
-//
-//public class Router<Destination: RouterDestination>: ObservableObject {
-//
-//    @Published public var modalsState = Array(repeating: Array(repeating: false, count: 100), count: 10)
-//    @Published public var activeModalsIndices = [[Int]]()
-//    @Published public var tabSelection = 0
-//
-//    @Published public var paths = Array(repeating: Array(repeating: NavigationPath(), count: 100), count: 10)
-//    @Published public var pathIndex = Array(repeating: 0, count: 10)
-//
-//
-//    // MARK: - push
-//
-//    /// Pushes a destination onto the router stack.
-//    ///
-//    /// - Parameters:
-//    ///   - destination: Destination view.
-//    ///   - completion: Callback trigerred when the destination finished pushing.
-//    public func push(_ destination: Destination, completion: @escaping () -> () = {}) {
-//        guard pathIndex[tabSelection] < paths[tabSelection].count else { return }
-//        DispatchQueue.main.async {
-//            self.paths[self.tabSelection][self.pathIndex[self.tabSelection]].append(destination)
-//        }
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.6, execute: {
-//            completion()
-//        })
-//    }
-//
-//    /// Pushes a destination onto the router stack asyncronously.
-//    ///
-//    /// - Parameter destination: Destination view.
-//    @MainActor
-//    public func push(_ destination: Destination) async {
-//        await withCheckedContinuation({ continuation in
-//            push(destination) {
-//                continuation.resume()
-//            }
-//        })
-//    }
-//
-//    // MARK: - present
-//
-//    /// Presents a destination as a modal on the router stack.
-//    ///
-//    /// - Parameters:
-//    ///   - destination: Destination view.
-//    ///   - completion: Callback trigerred when the destination finished presenting.
-//    public func present(_ destination: Destination, completion: @escaping () -> () = {}) {
-//        activeModalsIndices[tabSelection].append(destination.modalValue)
-//        DispatchQueue.main.async {
-//            self.modalsState[self.tabSelection][destination.modalValue] = true
-//        }
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.6, execute: {
-//            completion()
-//        })
-//    }
-//
-//    /// Presents a destination as a modal on the router stack asyncronously.
-//    ///
-//    /// - Parameter destination: Destination view.
-//    @MainActor
-//    public func present(_ destination: Destination) async {
-//        await withCheckedContinuation({ continuation in
-//            present(destination) {
-//                continuation.resume()
-//            }
-//        })
-//    }
-//
-//    // MARK: - pop
-//
-//    /// Pops a view from the router stack.
-//    ///
-//    /// - Parameters:
-//    ///   - type: The type of the pop.
-//    ///   - completion: Callback trigerred when the destination finished popping.
-//    public func pop(_ type: PopType = .one, completion: @escaping () -> () = {}) {
-//        switch type {
-//        case .one:
-//            if paths[tabSelection][pathIndex[tabSelection]].count == 0 {
-//                dismissModal(completion: completion)
-//            } else {
-//                dismiss(completion: completion)
-//            }
-//        case .the(let last):
-//            for i in 0..<last {
-//                DispatchQueue.main.asyncAfter(deadline: .now() + 0.6 * Double(i), execute: {
-//                    self.pop {
-//                        if i == last - 1 {
-//                            completion()
-//                        }
-//                    }
-//                })
-//            }
-//        case .to(let index):
-//            let all = getAllViewsCount()
-//            let last = all - index
-//            pop(.the(last: last), completion: completion)
-//        case .toRoot:
-//            let all = getAllViewsCount()
-//            let last = all
-//            pop(.the(last: last), completion: completion)
-//        }
-//    }
-//
-//    /// Pops a view from the router stack asyncronously.
-//    ///
-//    /// - Parameter type: The type of the pop.
-//    @MainActor
-//    public func pop(_ type: PopType = .one) async {
-//        await withCheckedContinuation({ continuation in
-//            pop(type) {
-//                continuation.resume()
-//            }
-//        })
-//    }
-//
-//    // MARK: - internal
-//
-//    internal func dismiss(completion: @escaping () -> ()) {
-//        guard pathIndex[tabSelection] < paths[tabSelection].count, !paths[tabSelection][pathIndex[tabSelection]].isEmpty else { return }
-//        DispatchQueue.main.async {
-//            self.paths[self.tabSelection][self.pathIndex[self.tabSelection]].removeLast()
-//        }
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.6, execute: {
-//            completion()
-//        })
-//    }
-//
-//    internal func dismissModal(completion: @escaping () -> () = {}) {
-//        guard let index = activeModalsIndices[tabSelection].last else { return }
-//        DispatchQueue.main.async {
-//            self.modalsState[self.tabSelection][index] = false
-//        }
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.6, execute: {
-//            completion()
-//        })
-//    }
-//
-//    internal func getAllViewsCount() -> Int {
-//        var count = 0
-//        paths[tabSelection].forEach { path in
-//            count += path.count
-//        }
-//        count += activeModalsIndices[tabSelection].count
-//        count -= 1
-//        return count
-//    }
-//
-//}
-//
-
-
 //
 //  Router.swift
 //
@@ -336,7 +169,15 @@ public class Router<Destination: RouterDestination>: ObservableObject {
         return count
     }
     
-    
+    internal func getPaths() -> [Int] {
+        var paths = [Int]()
+        self.paths[tabSelection].forEach { path in
+            if !path.isEmpty {
+                paths.append(path.count)
+            }
+        }
+        return paths.reversed()
+    }
     
     internal func popOne(completion: @escaping () -> ()) {
         if paths[tabSelection][pathIndex[tabSelection]].count == 0 {
@@ -366,7 +207,6 @@ public class Router<Destination: RouterDestination>: ObservableObject {
             let paths = getPaths()
             for i in 0..<paths.count {
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.6 * Double(i), execute: {
-                    print(self.activeModalsIndices[self.tabSelection])
                     if self.activeModalsIndices[self.tabSelection].count > paths.count {
                         self.dismissModal {
                             let last = last - 1
@@ -395,42 +235,6 @@ public class Router<Destination: RouterDestination>: ObservableObject {
     internal func popToRoot(style: PopStyle, completion: @escaping () -> ()) {
         let all = getAllViewsCount()
         popTheLast(all, style: style, completion: completion)
-//        switch style {
-//        case .oneByOne:
-//            let all = getAllViewsCount()
-//            popTheLast(all, style: style, completion: completion)
-//        case .shortest:
-//            let paths = getPaths()
-//            for i in 0..<paths.count {
-//                DispatchQueue.main.asyncAfter(deadline: .now() + 0.6 * Double(i), execute: {
-//                    print(self.activeModalsIndices[self.tabSelection])
-//                    if self.activeModalsIndices[self.tabSelection].count > paths.count {
-//                        self.dismissModal {
-//                            self.popToRoot(style: style, completion: completion)
-//                        }
-//                    } else {
-//                        self.dismissStack {
-//                            if i == paths.count - 1 {
-//                                completion()
-//                            } else {
-//                                self.popToRoot(style: style, completion: completion)
-//                            }
-//                        }
-//                    }
-//                })
-//            }
-//        }
-    }
-    
-    internal func getPaths() -> [Int] {
-        var paths = [Int]()
-        self.paths[tabSelection].forEach { path in
-            if !path.isEmpty {
-                print(path.count)
-                paths.append(path.count)
-            }
-        }
-        return paths.reversed()
     }
     
 }
